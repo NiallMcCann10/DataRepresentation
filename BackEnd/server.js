89a729f3ad92211c3d3f9795cc00300757dee982
Niall <niallmc>
2018-12-02 21:09:41 +0000

    Project

---
+++ BackEnd/server.js
@@ -0,0 +1,127 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://admin:123456a@ds137913.mlab.com:37913/niallmccann';
+mongoose.connect(mongoDB);
+
+var Schema = mongoose.Schema;
+var postSchema = new Schema({
+    name: String,
+    ContactDetails: String,
+    Package: String
+})
+var PostModel = mongoose.model('post', postSchema);
+
+
+//Here we are configuring express to use body-parser as middle-ware. 
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+    });
+    
+
+app.get('/', function (req, res) {
+   res.send('Hello from Express');
+})
+
+app.post('/api/posts', function(req, res){
+    console.log("post successful");
+    console.log(req.body.name);
+    console.log(req.body.ContactDetails);
+    console.log(req.body.Package);
+
+    PostModel.create({
+        name: req.body.name,
+        ContactDetails: req.body.ContactDetails,
+        Package: req.body.Package
+    });
+    res.send('Item added');
+
+
+})
+
+app.get('/api/posts', function(req, res){
+    PostModel.find(function(err, data){
+        res.json(data);
+    });
+})
+
+app.get('/api/posts/:id', function(req, res){
+    console.log("Read post " +req.params.id);
+
+    //PostModel.find({_id : req.params.id}, 
+    PostModel.findById(req.params.id,
+        function (err, data) {
+            res.json(data);
+        });
+})
+
+app.put('/api/posts/:id', function(req, res){
+    console.log("Update Post" +req.params.id);
+    console.log(req.body.name);
+    console.log(req.body.ContactDetails);
+    console.log(req.body.Package);
+
+    PostModel.findByIdAndUpdate(req.params.id, req.body, 
+        function(err, data){
+            res.send(data);
+        })
+})
+
+app.delete('/api/posts/:id', function(req, res){
+    console.log(req.params.id);
+
+    PostModel.deleteOne({_id:req.params.id},
+    function(err, data)
+    {
+        if(err)
+            res.send(err);
+        res.send(data);
+    })
+})
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+  
+    var bind = typeof port === 'string'
+      ? 'Pipe ' + port
+      : 'Port ' + port;
+  
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
+
+
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
